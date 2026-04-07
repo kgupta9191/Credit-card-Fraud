443f978b669b68a684c20a18e47f6dff1c2b8336
kgupta9191 <kgupta3@umassd.edu>
2026-04-07 18:37:07 -0400

    Implement data loading and MLP architecture
    Added data loading, preprocessing, and neural network architecture for credit card fraud detection.

--- model_weights.py
+++ model_weights.py
@@ -13,3 +13,49 @@ import torch.optim as optim
 from torch.utils.data import TensorDataset, DataLoader
 from torch.utils.data import random_split
 import numpy as np
+
+# Load data
+data = pd.read_csv("creditcard.csv")
+
+# Before creating tensor dataset, data cleaning is done which is shown in the report and not included in the code.
+# It requires multiple plotting and generating data-charts to have a sense of outliers and data balancing.
+# This required manual input. Once done this code can be easily implemented.
+
+conv_data = torch.tensor(
+    data.values,
+    dtype=torch.float32
+)
+
+# Split data
+X = conv_data[:, :-1]
+#y = data[:, -3:].unsqueeze(1)
+y = conv_data[:, -1]
+dataset = TensorDataset(X, y)
+N = len(dataset)
+n_train = int(0.8 * N)
+n_val   = int(0.1 * N)
+n_test  = N - n_train - n_val
+train_ds, val_ds, test_ds = random_split(
+    dataset,
+    [n_train, n_val, n_test],
+    generator=torch.Generator().manual_seed(42)  # reproducible
+)
+
+# Neural Architecture
+class MLP(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(28, 128),
+            nn.ReLU(),
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Linear(128, 1)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+
+
