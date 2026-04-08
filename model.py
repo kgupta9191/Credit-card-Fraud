30609669d9df9ae11c719ffa0294e9a2faec1647
kgupta9191 <kgupta3@umassd.edu>
2026-04-07 20:03:57 -0400

    Remove CUDA module loading and print statements
    Removed CUDA and cuDNN module loading and related print statements.

--- model.py
+++ model.py
@@ -1,12 +1,5 @@
 # Load Modules
-!module load cuda/12.6
-!module load cudnn/8.9.7.29-12-cuda12.6
 import torch
-print("CUDA version:", torch.version.cuda)
-print("cuDNN version:", torch.backends.cudnn.version())
-print("GPU available?", torch.cuda.is_available())
-print("Device count:", torch.cuda.device_count())
-print("Device name:", torch.cuda.get_device_name(0))
 import torch.nn as nn
 import pandas as pd
 import torch.optim as optim
